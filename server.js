d591d647b0347d0675a0482e6ee16988bc0ecb14
ceren-oss <ceren@miniskop.com.tr>
2026-05-14 16:12:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const REDIRECT_URI =
   "https://k12net-sso-production.up.railway.app/auth/k12net/callback";
 
 
-// ANA SAYFA TESTİ
+// ANA SAYFA
 app.get("/", (req, res) => {
   res.send("Backend çalışıyor 🚀");
 });
@@ -23,11 +23,13 @@ app.get("/", (req, res) => {
 app.get("/auth/k12net/login", (req, res) => {
 
   const authUrl =
-    `${BASE_URL}/oauth/authorize` +
-    `?client_id=${CLIENT_ID}` +
+    `${BASE_URL}/GWCore.Web/connect/authorize` +
+    `?response_type=code` +
+    `&client_id=${CLIENT_ID}` +
     `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
-    `&response_type=code` +
-    `&scope=openid profile email`;
+    `&scope=openid profile`;
+
+  console.log(authUrl);
 
   res.redirect(authUrl);
 });
@@ -46,7 +48,7 @@ app.get("/auth/k12net/callback", async (req, res) => {
 
     // TOKEN AL
     const tokenResponse = await axios.post(
-      `${BASE_URL}/oauth/token`,
+      `${BASE_URL}/GWCore.Web/connect/token`,
       new URLSearchParams({
         grant_type: "authorization_code",
         client_id: CLIENT_ID,
@@ -63,9 +65,11 @@ app.get("/auth/k12net/callback", async (req, res) => {
 
     const accessToken = tokenResponse.data.access_token;
 
+    console.log("ACCESS TOKEN:", accessToken);
+
     // USER INFO
     const userResponse = await axios.get(
-      `${BASE_URL}/userinfo`,
+      `${BASE_URL}/GWCore.Web/connect/userinfo`,
       {
         headers: {
           Authorization: `Bearer ${accessToken}`
