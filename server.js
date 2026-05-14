e14ef8471015f0ea1d41f50a382ca1d946589c12
ceren-oss <ceren@miniskop.com.tr>
2026-05-14 16:15:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,102 +1,10 @@
 const express = require("express");
-const axios = require("axios");
 
 const app = express();
 
-const CLIENT_ID = "47d21f52-0c07-495e-de49-5c4ae83af8ab";
-
-const CLIENT_SECRET = "0635f3f0-4f49-4e1c-c35b-84562b47d4b1";
-
-const BASE_URL = "https://api.k12net.com";
-
-const REDIRECT_URI =
-  "https://k12net-sso-production.up.railway.app/auth/k12net/callback";
-
-
-// ANA SAYFA
 app.get("/", (req, res) => {
   res.send("Backend çalışıyor 🚀");
 });
-
-
-// LOGIN
-app.get("/auth/k12net/login", (req, res) => {
-
-  const authUrl =
-    `${BASE_URL}/GWCore.Web/connect/authorize` +
-    `?response_type=code` +
-    `&client_id=${CLIENT_ID}` +
-    `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
-    `&scope=openid profile`;
-
-  console.log(authUrl);
-
-  res.redirect(authUrl);
-});
-
-
-// CALLBACK
-app.get("/auth/k12net/callback", async (req, res) => {
-
-  const code = req.query.code;
-
-  if (!code) {
-    return res.send("Authorization code bulunamadı");
-  }
-
-  try {
-
-    // TOKEN AL
-    const tokenResponse = await axios.post(
-      `${BASE_URL}/GWCore.Web/connect/token`,
-      new URLSearchParams({
-        grant_type: "authorization_code",
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        code: code,
-        redirect_uri: REDIRECT_URI
-      }),
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      }
-    );
-
-    const accessToken = tokenResponse.data.access_token;
-
-    console.log("ACCESS TOKEN:", accessToken);
-
-    // USER INFO
-    const userResponse = await axios.get(
-      `${BASE_URL}/GWCore.Web/connect/userinfo`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`
-        }
-      }
-    );
-
-    const user = userResponse.data;
-
-    console.log(user);
-
-    res.send(`
-      <h1>Giriş Başarılı 🎉</h1>
-      <pre>${JSON.stringify(user, null, 2)}</pre>
-    `);
-
-  } catch (error) {
-
-    console.log("HATA:", error.response?.data || error.message);
-
-    res.send(`
-      <h1>Hata Oluştu ❌</h1>
-      <pre>${JSON.stringify(error.response?.data || error.message, null, 2)}</pre>
-    `);
-  }
-});
-
 
 const PORT = process.env.PORT || 3000;
 
